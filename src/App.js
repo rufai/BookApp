7f54c3dcb41980e62bb4eb129f8782742a4302dc
Rufai Mustapha <etop94@gmail.com>
2022-02-02 22:17:12 +0100

    feat: data fetching

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import Book from './components/Book';
 import bookList from './model/books.json'
 import PropTypes from 'prop-types';
 import BookList from './components/BookList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Genre from './components/Genre';
 
 function App() {
 
+  useEffect(() => {
+    const term = 'food';
+    const url  = `https://www.googleapis.com/books/v1/volumes?q=${term}&filter=paid-ebook
+    s&print-type=books&projection=lite`;
+    const results =  fetch(url).then(res => res.json());
+    console.log(results)
+  },[])
+
+  const [name, setName] = useState("")
+
+  const previewName = (e) => {
+    setName(e.target.value)
+  }
+
+  const previewPassword = (e) => {
+    
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    alert('A name was submitted: ' + name);
+  }
   function helpMeCollect(data){
     console.log(data)
   }
@@ -38,18 +59,26 @@ function App() {
            </li>
            
          </ul>
-          <input id='search' name='search' type="search" placeholder='search for books' onChange={(e) => previewSearch(e)}/>
+         <input id='search' name='search' type="search" placeholder='search for books' onChange={(e) => previewSearch(e)}/>
+
+         <form onSubmit={(event) => handleSubmit(event)} >
+           <div>
+             <label>Username</label>
+            <input required id='search' name='search' type="text" placeholder='enter your username' onChange={(e) => previewName(e)}/>
+            </div>
+           <div>
+             <label>Password</label>
+            <input required id='search' name='search' type="password" placeholder='enter your password' onChange={(e) => previewPassword(e)}/>
+            </div>
+            <input type="submit" value="Sign in"/>
+          </form>
           <BookList color="orange" genre="romance" setHelpMeCollect={helpMeCollect}>    
               <h1>My Books {input}</h1>
               {books.map(bookItem => <Book book={bookItem} /> )}
           </BookList>
         </section>
 
-        <Routes>
-          <Route path="/genre">
-              <Genre />
-          </Route>
-        </Routes>
+    
     </Router>
 
   );
